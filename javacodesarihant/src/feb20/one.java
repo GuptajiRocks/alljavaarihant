78471ac3cf0845da8bb1c97a016b5b09cb5c804b
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-02-20 14:10:05 +0530

    trying changes.

--- javacodesarihant/src/feb20/one.java
+++ javacodesarihant/src/feb20/one.java
@@ -1,7 +1,17 @@
 package feb20;
 import java.util.Scanner;
+class database {
+
+}
 public class one {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String cname = sc.nextLine();
+
+
+        
+
+
 
     }
 }
